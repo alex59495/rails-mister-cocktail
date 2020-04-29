5e9d393eee57c2417468d93da5164e9960ca3e52
Alexis Lenoir <alexis59495@gmail.com>
2020-04-29 12:45:29 -0300

    Add a validation on the image

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,6 +3,7 @@ class Cocktail < ApplicationRecord
     has_many :ingredients, through: :doses
     validates :name, presence: true, uniqueness: { message: "This cocktail already exists" }
     has_one_attached :cocktail_image
+    validates :cocktail_image, presence: { message: "You must add an image" }
 
     def self.search(search)
         if search
