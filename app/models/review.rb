2c3b4ba1ae251da09b4429777f23a8971dbdf986
Alexis Lenoir <alexis59495@gmail.com>
2020-05-01 17:23:39 -0300

    Delete the 0 option in the rate

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :cocktail
-  validates :rate, inclusion: { in: (0..5) }, presence: true
+  validates :rate, inclusion: { in: (1..5) }, presence: true
   validates :content, presence: true
 end
