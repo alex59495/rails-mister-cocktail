d155d53f23ce0d2b0903a532f959bfe30024936a
Alexis Lenoir <alexis59495@gmail.com>
2020-05-06 17:10:03 -0300

    Correct the current url path

--- app/javascript/components/card.js
+++ app/javascript/components/card.js
@@ -1,14 +1,13 @@
 const ClickOnCocktailCard = () => {
-  const url = 'http://localhost:3000/cocktails/';
+  const url = window.location.href;
+  console.log(url);
   const card_cocktail = document.querySelectorAll(".card-cocktail");
   if (card_cocktail.length != 0) {
     addEventListener('click', (event) => {
       const cocktail_id = event.path[0].firstElementChild.lastElementChild.innerHTML;
       window.location = url + cocktail_id;
     })
-    
   }
-
 }
 
 export { ClickOnCocktailCard };
